81e0fc6e3734d1cf0fd675cc39e4df926eec572f
unknown <davidwu@mit.edu>
2016-03-01 15:19:16 -0500

    Added landMine/explosion

---
+++ Assets/Mine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour {
+
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Stepped on Mine");
+		Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+		Destroy (gameObject);
+
+	}
+}
---
+++ Assets/explosionController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosionController : MonoBehaviour {
+
+	public int radius;
+	private GameObject[] men;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("instantiated");
+		men = GameObject.FindGameObjectsWithTag("man");
+		StartCoroutine (explode ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject man in men) {
+			if (man != null) {
+				float distanceSqr = (gameObject.transform.position - man.transform.position).sqrMagnitude;
+				if (distanceSqr < radius) {
+					Destroy (man);
+				}
+			}
+		}
+	}
+
+	IEnumerator explode()
+	{
+		yield return new WaitForSeconds (2);
+		Destroy (gameObject);
+	}
+}
