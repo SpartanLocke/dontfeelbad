5a3816fcb319798f0b6731b057444de8227183af
unknown <davidwu@mit.edu>
2016-03-05 22:29:00 -0500

    sort of functional car movement with traffic lights - cars limited atm to straight lines in one direction. No traffic light interaction with people movement yet

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+	public Transform[] wayPointList;
+
+	public int currentWayPoint = 0;
+	public bool vertical;
+	Transform targetWayPoint;
+	public GameObject stopLight;
+
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log(currentWayPoint);
+		// check if we have somewere to walk
+		if (currentWayPoint < this.wayPointList.Length)
+		{
+			if (targetWayPoint == null) targetWayPoint = wayPointList[currentWayPoint];
+			//Check if at stoplight
+			// Probably should change this to a tag on the waypoint later
+			if (vertical && transform.position.y == wayPointList [1].position.y && !stopLight.GetComponent<TrafficLight> ().isGreen ()) {
+			} else if (!vertical && transform.position.x == wayPointList [1].position.x && !stopLight.GetComponent<TrafficLight> ().isGreen ()) {
+			} else {
+				drive ();
+			}
+		}
+	}
+
+	void drive()
+	{
+		// rotate towards the target
+		//transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+
+		// move towards the target
+		transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+		if (transform.position == targetWayPoint.position)
+		{
+			currentWayPoint++;
+			if (currentWayPoint >= wayPointList.Length)
+			{
+				currentWayPoint = 0;
+				transform.position = wayPointList [currentWayPoint].position;
+			}
+			targetWayPoint = wayPointList[currentWayPoint];
+		}
+	}
+}
---
+++ Assets/TrafficLight.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficLight : MonoBehaviour {
+
+    
+	public bool northSouth;
+	private bool broken;
+	private int state = 0;
+   	void Start()
+    {
+		broken = false;
+     StartCoroutine(setLight());
+     
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool isGreen()
+	{
+		return (state == 0);
+	}
+
+	IEnumerator setLight()
+	{
+		
+    while(true && !broken)
+    {
+			if (northSouth) {
+				Debug.Log ("In here");
+				state = 0;
+				GetComponent<Renderer> ().material.color = new Color (0, 1, 0);
+				yield return new WaitForSeconds (3);
+				state = 1;
+				GetComponent<Renderer> ().material.color = new Color (1, 1, 0);
+				yield return new WaitForSeconds (3);
+				state = 2;
+				GetComponent<Renderer> ().material.color = new Color (1, 0, 0);
+				yield return new WaitForSeconds (6);
+			} else {
+				Debug.Log ("In here");
+				state = 2;
+				GetComponent<Renderer> ().material.color = new Color (1, 0, 0);
+				yield return new WaitForSeconds (6);
+				state = 0;
+				GetComponent<Renderer> ().material.color = new Color (0, 1, 0);
+				yield return new WaitForSeconds (3);
+				state = 1;
+				GetComponent<Renderer> ().material.color = new Color (1, 1, 0);
+				yield return new WaitForSeconds (3);
+
+			}
+    }
+
+	}
+}
