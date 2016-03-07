f121fef453d1ac91608c4c344add42c4706e2f7d
William Wong <souleni5k@dhcp-18-189-101-130.dyn.mit.edu>
2016-03-07 14:07:30 -0500

    thought bubbles + random colors

---
+++ Assets/randomColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int n = Random.Range (0, 6);
+		Color[] colors = new Color[] { Color.black, Color.blue, Color.green, Color.red, Color.yellow, Color.white};
+		gameObject.GetComponent<SpriteRenderer> ().color = colors[n];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
